0e0de7c182dfe7903e4545dcc9507f1ee0c63c13
Kostis Karantias <karantiaskostis@gmail.com>
2012-11-03 08:20:50 -0400

    Replace obsolete variables.

--- js/graph.js
+++ js/graph.js
@@ -13,15 +13,15 @@ var Graph = function( minx, maxx, miny, maxy, left, top, width, height ) {
 };
 
 Graph.prototype.transformX = function( x ) {
-    var padX = W / ( this.maxx - this.minx ),
-        xx = padX * x;
+    var padX = this.width / ( this.maxx - this.minx );
+    var xx = padX * x;
 
     return xx + this.left;
 };
 
 Graph.prototype.transformY = function( y ) {
-    var padY = H / ( this.maxy - this.miny ),
-        yy = padY * y;
+    var padY = this.height / ( this.maxy - this.miny );
+    var yy = padY * y;
     
     return this.height - yy + this.top;
 };
